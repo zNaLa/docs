167dd27d6167a71d86ebfc65a7904d5fdb3cd11e
Repulser <thetechproject2014@gmail.com>
2020-04-02 20:03:35 +0300

    Push working version

---
+++ docusaurus.config.js
@@ -0,0 +1,45 @@
+module.exports = {
+  title: 'Rythm',
+  tagline: 'Rythm documentation',
+  url: 'https://docs.rythmbot.co/',
+  baseUrl: '/',
+  favicon: 'img/favicon.ico',
+  organizationName: 'Rythm', // Usually your GitHub org/user name.
+  projectName: 'Rythm-documentation', // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: 'Rythm Documentation',
+      logo: {
+        alt: 'rythmlogo',
+        src: 'img/logo.svg',
+      },
+      links: [
+        {
+          href: 'https://github.com/Repulser/Rythm-documentation',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [],
+      copyright: `Copyright © ${new Date().getFullYear()} Rythm. Built with Docusaurus.`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          routeBasePath: '',
+          sidebarPath: require.resolve('./sidebars.js'),
+          editUrl: 'https://github.com/Repulser/Rythm-documentation/edit/master/website/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
