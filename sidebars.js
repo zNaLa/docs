e76e3572220307e11154ab0606496b24e9bad061
HmmOrange <leanhduy0206@gmail.com>
2021-03-27 18:38:26 +0000

    voice region thingy

--- sidebars.js
+++ sidebars.js
@@ -1,10 +1,11 @@
 module.exports = {
   someSidebar: {
-    'Getting Started': ['start', 'adding_rythm', 'play_song', 'queue'],
-    'Basics of Rythm': ['features', 'commands', 'faq'],
+    'Introduction': ['start', 'features'],
+    'Getting Started': ['adding_rythm', 'play_song', 'queue'],
+    'Basics of Rythm': ['commands', 'slash_command', 'faq'],
     'Advanced Usage': [`dj_role`, `voteskip`, `koodos`, 'permissions',  'settings'],
     'Dashboard': ['dashboard_overview', 'dashboard_usage', `dashboard_troubleshooting`, `dashboard_app`],
-    'Premium': ['premium'],
+    'Premium': ['premium', 'premium_faq'],
     'Rythm Troubleshooting': ['invite_troubleshooting', 'basic_troubleshooting', 'audio_troubleshooting', 'mobile_troubleshooting', 'get_support'],
     'Discord Guides': ['server_region', 'server_id'],
   },
