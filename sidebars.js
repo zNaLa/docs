569295c8fb0bce1689019865fca5817151f175c7
HmmOrange <leanhduy0206@gmail.com>
2021-03-28 01:42:07 +0700

    did an oopsie

--- sidebars.js
+++ sidebars.js
@@ -2,11 +2,11 @@ module.exports = {
   someSidebar: {
     'Introduction': ['start', 'features'],
     'Getting Started': ['adding_rythm', 'play_song', 'queue'],
-    'Basics of Rythm': ['commands', 'slash_command', 'faq'],
+    'Basics of Rythm': ['commands', 'faq'],
     'Advanced Usage': [`dj_role`, `voteskip`, `koodos`, 'permissions',  'settings'],
     'Dashboard': ['dashboard_overview', 'dashboard_usage', `dashboard_troubleshooting`, `dashboard_app`],
-    'Premium': ['premium', 'premium_faq'],
+    'Premium': ['premium'],
     'Rythm Troubleshooting': ['invite_troubleshooting', 'basic_troubleshooting', 'audio_troubleshooting', 'mobile_troubleshooting', 'get_support'],
-    'Discord Guides': ['server_region', 'server_id'],
+    'Discord Guides': ['voice_region', 'server_id'],
   },
 };
